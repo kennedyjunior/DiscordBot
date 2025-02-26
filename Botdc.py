38738505db93b7c297827b25d0bb284c7cf1ca9f
Kennedy Junior <josekennedy37@gmail.com>
2025-02-25 21:44:05 -0300

    Update Botdc.py

--- Botdc.py
+++ Botdc.py
@@ -1,6 +1,6 @@
 import os
-import discord
-from discord import app_commands
+import discord.py
+from discord.py import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
